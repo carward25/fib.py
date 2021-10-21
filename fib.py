e1801b4bfbdb37281873795376059edd74f9ec24
carward25 <83989145+carward25@users.noreply.github.com>
2021-10-20 23:08:16 -0700

    Update fib.py

--- fib.py
+++ fib.py
@@ -4,12 +4,4 @@
 #positive integer parameter and returns the number at that position of the Fibonacci sequence
 
 def fib(n):
-   # if(n==1):
-       # return 1
-   # elif(n==2):
-       # return 1
-   # else:
-      #  lst = [1,1]
-       # for i in range(2,n):
-        #    lst.append(lst[i-1]+lst[i-2])
-       # return lst[n-1]
+
